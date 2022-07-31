635224309dc799cd6c2c3057567504476a312a60
Armando.Rod <armando.rod79@gmail.com>
2022-07-31 12:25:59 -0700

    updated bank of questions in .js file

--- script.js
+++ script.js
@@ -9,22 +9,58 @@ let score = 0
 let timer = 75
 let currentQuestionIndex = 0;
 
-
+// Bank of questions
 let questions = [
     {
-        question: "What is the color of the sky",
-        options: ["blue", "orange", "red", "green"],
-        answer: "blue"
+        question: "Javascript is an _______ language?",
+        options: ["A. Object-Oriented", "B. Object-Based", "C. Procedural", "D. None of the Above"],
+        answer: "A"
     },
     {
-        question: "What car is best",
-        options: ["audi", "ford", "kia", "gmc"],
-        answer: "kia"
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        options: ["A. Var", "B. Let", "C. Both A & B", "D. None of the Above"],
+        answer: "C"
     },
     {
-        question: "What language is best",
-        options: ["java", "c++", "javascript", "puthon"],
-        answer: "javascript"
+        question: "Which of the following methods is used to access HTML elements using Javascript?",
+        options: ["A. getElementbyID", "B. getElementsByClassName()", "C. Both A & B", "D. None of the Above"],
+        answer: "C"
+    }
+
+    {
+        question: "What does the ‘toLocateString()’ method do in JS?",
+        options: ["A. Returns a localised object representation.", "B. Returns a parsed string.", "C. Returns a localised string representation of an objecct. ", "D. None of the Above"],
+        answer: "C"
+    }
+    {
+        question: "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+        options: ["A. Boolean", "B. Undefined", "C. Object", "D. Integer"],
+        answer: "C"
+    }
+    {
+        question: "What keyword is used to check whether a given property is valid or not?",
+        options: ["A. in", "B. is in", "C. exists", "D. lies"],
+        answer: "A"
+    }
+    {
+        question: "When the switch statement matches the expression with the given labels, how is the comparison done?",
+        options: ["A. Only the datatype of the expression is compared", "B. Both the datatype and the result of the expression are compared", "C. Only the value of the expression is compared", "D. None of the Above"],
+        answer: "B"
+    }
+    {
+        question: "How can a datatype be declared to be a constant type?",
+        options: ["A. var", "B. let", "C. constant", "D. const"],
+        answer: "D"
+    }
+    {
+        question: "Which of the following methods can be used to display data in some form using Javascript?",
+        options: ["A. document.write()", "B. console.log()", "C. window.alert()", "D. All of the Above"],
+        answer: "D"
+    }
+    {
+        question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+        options: ["A. Throws an error", "B. Ignores the statements)", "C. Gives a warning", "D. None of the Above"],
+        answer: "B"
     }
 ];
 
